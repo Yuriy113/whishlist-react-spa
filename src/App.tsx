7e282d41185301df7caad577399de34eacc939ce
Yuriy Ulanov <1859697@gmail.com>
2026-03-05 23:58:22 +0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+function App() {  
+  return (
+    <>
+      <h1>My wishlist</h1>
+
+      <div>
+        <button>Новое желание</button>
+      </div>
+      <div>
+        <button>Создать вишлист</button>
+      </div>
+      <div>
+        <button>Мои вишлисты</button>
+      </div>
+      <div>
+        <button>Доступные мне</button>
+      </div>
+    </>
+  )
+}
+
+export default App
